913aba3680f64f62a31fb37e9270642fde0d60e4
JustinasZailskas <justinaszailskas@gmail.com>
2026-03-03 15:40:46 +0200

    Masyvo elemento salinimas

--- masyvai1/main.cpp
+++ masyvai1/main.cpp
@@ -30,29 +30,65 @@ int main() {
     //     cout << i+1 << " elemento reiksme yra lygi: "<<naujasSkaiciuMasyvas[i]<<" indeksas yra "<<i<<endl;
     // }
 
-    int masyvas[10];
-    int elemKiekis, nuliuKiekis = 0;
-
-    cout<<"Iveskite masyvo elementu skaiciu: "<<endl;
-    cin >> elemKiekis;
-    cout << "Iveskite masyvo elementu reiksmes"<<endl;
-    for (int i = 0; i < elemKiekis; i++) {
-        cout<<i+1<<" - aji masyvo elementas"<<endl;
-        cin >> masyvas[i];
-    }
-    for (int i = 0; i < elemKiekis; i++) {
-        cout << i+1 << " elemento reiksme yra lygi: "<<masyvas[i]<<" indeksas yra "<<i<<endl;
-        if(masyvas[i] == 0) {
-            nuliuKiekis++;
-        }
-    }
-
+    // int masyvas[10];
+    // int elemKiekis, nuliuKiekis = 0;
+    //
+    // cout<<"Iveskite masyvo elementu skaiciu: "<<endl;
+    // cin >> elemKiekis;
+    // cout << "Iveskite masyvo elementu reiksmes"<<endl;
     // for (int i = 0; i < elemKiekis; i++) {
+    //     cout<<i+1<<" - aji masyvo elementas"<<endl;
+    //     cin >> masyvas[i];
+    // }
+    // for (int i = 0; i < elemKiekis; i++) {
+    //     cout << i+1 << " elemento reiksme yra lygi: "<<masyvas[i]<<" indeksas yra "<<i<<endl;
     //     if(masyvas[i] == 0) {
     //         nuliuKiekis++;
     //     }
     // }
+    //
+    // // for (int i = 0; i < elemKiekis; i++) {
+    // //     if(masyvas[i] == 0) {
+    // //         nuliuKiekis++;
+    // //     }
+    // // }
+    //
+    // cout<<"Nuliu kiekis yra: "<<nuliuKiekis<<endl;
 
-    cout<<"Nuliu kiekis yra: "<<nuliuKiekis<<endl;
+    int masyvas[100];
+    int masyvoIlgis, pozicija;
+
+    cout<<"Masyvo ilgis: "<<endl;
+    cin >> masyvoIlgis;
+
+    cout<<"Iveskite masyvo reiksmes"<<endl;
+
+    for(int i = 0; i < masyvoIlgis; i++) {
+        cout<<i+1<<" - aji masyvo elementas"<<endl;
+        cin >> masyvas[i];
+    }
+
+    for (int i = 0; i < masyvoIlgis; i++) {
+        cout << i+1 << " elemento reiksme yra lygi: "<<masyvas[i]<<" indeksas yra "<<i<<endl;
+    }
+
+    cout<<"Iveskite pozicija kuria norite istrinti: "<<endl;
+    cin >> pozicija;
+
+    //  1   2   3   4   5   6  pries trinant elementa
+    //  0   1   2   3   4   5   indeksai
+    //  1   3   4   5   6       po trynimo
+    //  0   1   2   3   4       indeksai po trynimo
+    cout <<"==========Vyksta trynimas==========" << endl;
+    for (int i = pozicija - 1; i < masyvoIlgis; i++) {   //i = 2 - 1
+        masyvas[i] = masyvas[i + 1];                     //masyvas[1] = 2 jisai pasikeis i ta reiksme kuri yra masyvas[2] = 3 reiksme
+    }
+    masyvoIlgis--;
+    cout <<"==========Po trynimo==========" << endl;
+    for (int i = 0; i < masyvoIlgis; i++) {
+        cout << i+1 << " elemento reiksme yra lygi: "<<masyvas[i]<<" indeksas yra "<<i<<endl;
+    }
+
+
     return 0;
 }
