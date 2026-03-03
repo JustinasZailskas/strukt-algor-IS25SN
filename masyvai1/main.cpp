4e2838ad893766f3fa9e71856964857ee6309d5c
JustinasZailskas <justinaszailskas@gmail.com>
2026-03-03 14:59:18 +0200

    Masyvo elemento reiksmiu priskyrimas ir ju isvedimas

---
+++ masyvai1/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // int skaiciai[5];
+    // skaiciai[3] = 15;
+    // skaiciai[0] = 4;
+    // skaiciai[1] = 2;
+    // skaiciai[2] = skaiciai[0] + skaiciai[1];
+    // skaiciai[4] = skaiciai[2] * skaiciai[3];
+    //
+    // cout << skaiciai[0] << endl;
+    // cout << skaiciai[1] << endl;
+    // cout << skaiciai[2] << endl;
+    // cout << skaiciai[3] << endl;
+    // cout << skaiciai[4] << endl;
+    //
+    // for (int i = 0; i < 5; i++) {
+    //     cout << i+1 << " elemento reiksme yra lygi: "<<skaiciai[i]<<" indeksas yra "<<i<<endl;
+    // }
+
+    return 0;
+}
