32dee2236113dee818c844dd53799c42c4cf6363
JustinasZailskas <justinaszailskas@gmail.com>
2026-03-04 15:02:39 +0200

    Rasti stulpelio elementu suma, rasti stulpelio max elementa

--- dvimaciai-masyvai/main.cpp
+++ dvimaciai-masyvai/main.cpp
@@ -28,5 +28,24 @@ int main() {
         }
         cout << "Eilutes "<< i + 1 <<" suma: "<< eilutesSuma << endl;
     }
+
+    for (int j = 0; j < stulpelis; j++) {
+        int stulpelioSuma = 0;
+        for (int i = 0; i < eilutes; i++) {
+            stulpelioSuma = stulpelioSuma + masyvas[i][j];
+        }
+        cout << "Stulpelio "<< j + 1 <<" suma: "<< stulpelioSuma << endl;
+    }
+
+    for (int j = 0; j < stulpelis; j++) {
+        int maxStulpElem = masyvas[0][j];
+        for (int i = 1; i < eilutes; i++) {
+            if (maxStulpElem < masyvas[i][j]) {
+                maxStulpElem = masyvas[i][j];
+            }
+        }
+        cout << "Stulpelio "<< j + 1 <<" didziausias elementas yra: "<< maxStulpElem << endl;
+    }
+
     return 0;
 }
