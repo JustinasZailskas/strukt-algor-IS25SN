347f504b48fb2048e3482f2d6f69ad5c3f1e794c
JustinasZailskas <justinaszailskas@gmail.com>
2026-03-05 13:08:37 +0200

    Sukurtos funkcijos vidurkiui ir didziausiam ivertinimui rasti

--- dvimaciai-masyvai-pvz/main.cpp
+++ dvimaciai-masyvai-pvz/main.cpp
@@ -4,18 +4,21 @@
 
 using namespace std;
 
+double skaiciuotiVidurki(int pazymiai[], int kiekis);
+int rastiDizdiausiaPazymi(int pazymiai[], int kiekis);
+
 int main() {
 
     const int STUDENTAI = 3;
-    const int DALYKAI = 3;
+    const int DALYKAI = 4;
 
     string studentai[STUDENTAI] = {"Jonas", "Mantas", "Ieva"};
-    string dalykai[DALYKAI] = {"Matematika", "Programavimas", "Fizika"};
+    string dalykai[DALYKAI] = {"Matematika", "Programavimas", "Fizika", "Anglu"};
 
     int pazymiai[STUDENTAI][DALYKAI] = {
-        {8, 9, 7},
-        {6, 8, 9},
-        {10, 9, 8}
+        {8, 9, 7, 10},
+        {6, 8, 9, 8},
+        {10, 9, 8, 5}
     };
 
     cout << setw(15)<<left << "Studentas"; // Studentas
@@ -25,21 +28,42 @@ int main() {
     }
 
     cout << setw(15) <<"Vidurkis";
+    cout << setw(25) <<"Didziausias pazymys";
     cout << endl;
 
     for (int j = 0; j < STUDENTAI; j++) {
         cout << setw(15)<<left << studentai[j];
-        int suma = 0;
+
         for (int i = 0; i < DALYKAI; i++) {
             cout << setw(15)<<left << pazymiai[j][i];
-            suma += pazymiai[j][i];
         }
-        double vidurkis = (double)suma / DALYKAI;
+
+        double vidurkis = skaiciuotiVidurki(pazymiai[j], DALYKAI);
+        int maksPzymys = rastiDizdiausiaPazymi(pazymiai[j], DALYKAI);
         cout << setw(15)<<fixed<<setprecision(2) << vidurkis;
-        cout << endl;
+        cout << setw(25)<< maksPzymys << endl;
     }
-
-
 
     return 0;
 }
+
+double skaiciuotiVidurki(int pazymiai[], int kiekis) {
+    int suma = 0;
+    for (int i = 0; i < kiekis; i++) {
+        suma += pazymiai[i];
+    }
+    return (double) suma / (double) kiekis;
+}
+
+int rastiDizdiausiaPazymi(int pazymiai[], int kiekis) {
+    int maksPazymys = pazymiai[0];
+
+    for( int i = 1; i < kiekis; i++) {
+        if(pazymiai[i] > maksPazymys) {
+            maksPazymys = pazymiai[i];
+        }
+    }
+
+    return maksPazymys;
+}
+
